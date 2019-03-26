1073501487f5ae0c68043369ec8651fd5af631b6
Alex Lee <alex@uniteus.com>
2019-03-26 01:27:37 -0400

    Example hoverable menu item

--- src/App.js
+++ src/App.js
@@ -1,96 +1,30 @@
 import React, { Component } from 'react';
-import Button from 'react-bulma-components/lib/components/button';
-import Columns from 'react-bulma-components/lib/components/columns'
+import Menu from 'react-bulma-components/lib/components/menu';
 import './App.sass';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <div>
-          <h1>Gapless</h1>
-          <Columns gapless>
-            <Columns.Column size={1}>
-              <p className="bd-notification is-success">size-1</p>
-            </Columns.Column>
-            <Columns.Column size={1}>
-              <p className="bd-notification is-info">size-1</p>
-            </Columns.Column>
-            <Columns.Column size={1}>
-              <p className="bd-notification is-warning">size-1</p>
-            </Columns.Column>
-            <Columns.Column size={1}>
-              <p className="bd-notification is-danger">size-1</p>
-            </Columns.Column>
-            <Columns.Column size={1}>
-              <p className="bd-notification is-white">size-1</p>
-            </Columns.Column>
-            <Columns.Column size={1}>
-              <p className="bd-notification is-light">size-1</p>
-            </Columns.Column>
-            <Columns.Column size={1}>
-              <p className="bd-notification is-dark">size-1</p>
-            </Columns.Column>
-            <Columns.Column size={1}>
-              <p className="bd-notification is-black">size-1</p>
-            </Columns.Column>
-            <Columns.Column size={1}>
-              <p className="bd-notification is-dark">size-1</p>
-            </Columns.Column>
-            <Columns.Column size={1}>
-              <p className="bd-notification is-light">size-1</p>
-            </Columns.Column>
-            <Columns.Column size={1}>
-              <p className="bd-notification is-white">size-1</p>
-            </Columns.Column>
-            <Columns.Column size={1}>
-              <p className="bd-notification is-danger">size-1</p>
-            </Columns.Column>
-          </Columns>
-        </div>
-        <div>
-          <h1>Not Gapless</h1>
-          <Columns>
-            <Columns.Column size={1}>
-              <p className="bd-notification is-success">size-1</p>
-            </Columns.Column>
-            <Columns.Column size={1}>
-              <p className="bd-notification is-info">size-1</p>
-            </Columns.Column>
-            <Columns.Column size={1}>
-              <p className="bd-notification is-warning">size-1</p>
-            </Columns.Column>
-            <Columns.Column size={1}>
-              <p className="bd-notification is-danger">size-1</p>
-            </Columns.Column>
-            <Columns.Column size={1}>
-              <p className="bd-notification is-white">size-1</p>
-            </Columns.Column>
-            <Columns.Column size={1}>
-              <p className="bd-notification is-light">size-1</p>
-            </Columns.Column>
-            <Columns.Column size={1}>
-              <p className="bd-notification is-dark">size-1</p>
-            </Columns.Column>
-            <Columns.Column size={1}>
-              <p className="bd-notification is-black">size-1</p>
-            </Columns.Column>
-            <Columns.Column size={1}>
-              <p className="bd-notification is-dark">size-1</p>
-            </Columns.Column>
-            <Columns.Column size={1}>
-              <p className="bd-notification is-light">size-1</p>
-            </Columns.Column>
-            <Columns.Column size={1}>
-              <p className="bd-notification is-white">size-1</p>
-            </Columns.Column>
-            <Columns.Column size={1}>
-              <p className="bd-notification is-danger">size-1</p>
-            </Columns.Column>
-          </Columns>
-        </div>
-      </div>
-    );
+      <React.Fragment>
+        <Menu>
+          <Menu.List title="General">
+            <Menu.List.Item>Dashboard</Menu.List.Item>
+            <Menu.List.Item>Customer</Menu.List.Item>
+          </Menu.List>
+        </Menu>
+        <Menu>
+          <Menu.List title="General">
+            <Menu.List.Item>
+              <p>Dashboard (p tag) - NOT HOVERABLE</p>
+            </Menu.List.Item>
+            <Menu.List.Item>
+              <p>Customer (p tag) - NOT HOVERABLE</p>
+            </Menu.List.Item>
+          </Menu.List>
+        </Menu>
+
+      </React.Fragment>
+    )
   }
 }
 
